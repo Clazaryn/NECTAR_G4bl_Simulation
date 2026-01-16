753768837927d896550f00c0cf5f0b05ac8e3300
Guy Leckenby <leckenby@borlin305.cenbg.in2p3.fr>
2026-01-16 18:04:25 +0100

    Committing of changed reaction files, not just names lol.

--- bscript-GenEvents.sh
+++ bscript-GenEvents.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-reaction=$(grep '^reaction' reac_info_nChamb.txt | awk -F'=' '{gsub(/^ +| +$/,"",$2); print $2}' | awk '{print $1}')
+# Read reaction from reac_info.txt
+reaction=$(grep '^reaction' reac_info.txt | awk -F'=' '{gsub(/^ +| +$/,"",$2); print $2}' | awk '{print $1}')
 
 echo "   ####################################################   "
 echo "                                                          "
@@ -13,26 +14,29 @@ echo "   ####################################################   "
 ### User input parameters ###
 read -p "Enter number of ejectile events: " nevents < /dev/tty
 	
-# RUNS IN PARALLEL over the excitation energy range defined in test_event_Eexc.py and TreeAnalysis_eexc.C
-# Choose the range i and number of cores N to run over
+### RUNS IN PARALLEL over the excitation energy range defined in reac_info.txt ###
 N=12  # number of concurrent jobs
 
-# Read excitation energies from reac_info_nChamb.txt
-excitation_Ens=($(grep '^recoil_excEns' reac_info_nChamb.txt | awk -F'=' '{print $2}' | tr ',' ' '))
+# Read excitation energies from reac_info.txt
+excitation_Ens=($(grep '^recoil_excEns' reac_info.txt | awk -F'=' '{print $2}' | tr ',' ' '))
 echo "Excitation energies: ${excitation_Ens[@]}"
 
-# 206Pb(d,p): g = 0-9 MeV, 1n = 6.7-17 MeV, 2n = 15-23 MeV, 3n = 21.5-30 MeV
-# 206Pb(p,p')/(d,d'): g = 0-10 MeV, 1n = 8-17 MeV, 2n = 15-20 MeV
+# 206Pb compound nucleus: g = 0-9 MeV, 1n = 6.7-17 MeV, 2n = 15-23 MeV, 3n = 21.5-30 MeV
+# 205Pb(d,p) at 13 MeV/u populates up to 30 MeV, 206Pb(p,p') at 20 MeV/u populates up to 15 MeV
 # \!/\!/\!/ ------- DISABLE HR3n for (p,p') or (d,d'), otherwise the job hangs!!! ------- \!/\!/\!/
-if [ "$reaction" == "206Pbdp" ]; then
-  HRg_start=0;    HRg_stop=9;
-  HR1n_start=7;   HR1n_stop=17;
-  HR2n_start=15;  HR2n_stop=23;
-  HR3n_start=22;   HR3n_stop=26;
-elif [[ "$reaction" == "206Pbdd" || "$reaction" == "206Pbpp" ]]; then
+if [ "$reaction" == "205Pbdp" ]; then
   HRg_start=0;    HRg_stop=10;
   HR1n_start=8;   HR1n_stop=17;
-  HR2n_start=15;  HR2n_stop=20;
+  HR2n_start=15;  HR2n_stop=23;
+  HR3n_start=23;  HR3n_stop=30;
+elif [[ "$reaction" == "205Pbdd"]]; then
+  HRg_start=0;    HRg_stop=10;
+  HR1n_start=8;   HR1n_stop=17;
+  HR2n_start=15;  HR2n_stop=19;
+elif [[ "$reaction" == "206Pbpp" ]]; then
+  HRg_start=0;    HRg_stop=10;
+  HR1n_start=8;   HR1n_stop=15;
+  HR2n_start=15;  HR2n_stop=15;
 fi
 
 jobs_running=0	# Initialize job count	
@@ -51,9 +55,9 @@ for i in $(seq $HRg_start $HRg_stop); do		# HRg block
   jobs_running=$((jobs_running + 1))		# Throttle the number of parallel jobs
 
   if [ "$jobs_running" -ge "$N" ]; then
-    wait ${job_pids[0]}						# Wait for the first job to finish (blocking wait for one job)
+    wait ${job_pids[0]}						      # Wait for the first job to finish (blocking wait for one job)
     job_pids=("${job_pids[@]:1}")  			# Remove the finished job's PID from the array
-    jobs_running=$((jobs_running - 1))  	# Decrease the job count after one finishes
+    jobs_running=$((jobs_running - 1))  # Decrease the job count after one finishes
   fi
 done
 
@@ -70,9 +74,9 @@ for i in $(seq $HR1n_start $HR1n_stop); do		# HR1n block
   jobs_running=$((jobs_running + 1))		# Throttle the number of parallel jobs
 
   if [ "$jobs_running" -ge "$N" ]; then
-    wait ${job_pids[0]}						# Wait for the first job to finish (blocking wait for one job)
+    wait ${job_pids[0]}						      # Wait for the first job to finish (blocking wait for one job)
     job_pids=("${job_pids[@]:1}")  			# Remove the finished job's PID from the array
-    jobs_running=$((jobs_running - 1))  	# Decrease the job count after one finishes
+    jobs_running=$((jobs_running - 1))  # Decrease the job count after one finishes
   fi
 done
 
@@ -89,9 +93,9 @@ for i in $(seq $HR2n_start $HR2n_stop); do		# HR2n block
   jobs_running=$((jobs_running + 1))		# Throttle the number of parallel jobs
 
   if [ "$jobs_running" -ge "$N" ]; then
-    wait ${job_pids[0]}						# Wait for the first job to finish (blocking wait for one job)
+    wait ${job_pids[0]}						      # Wait for the first job to finish (blocking wait for one job)
     job_pids=("${job_pids[@]:1}")  			# Remove the finished job's PID from the array
-    jobs_running=$((jobs_running - 1))  	# Decrease the job count after one finishes
+    jobs_running=$((jobs_running - 1))  # Decrease the job count after one finishes
   fi
 done
 
@@ -109,9 +113,9 @@ if [ "$reaction" == "206Pbdp" ]; then
     jobs_running=$((jobs_running + 1))		# Throttle the number of parallel jobs
 
     if [ "$jobs_running" -ge "$N" ]; then
-      wait ${job_pids[0]}						# Wait for the first job to finish (blocking wait for one job)
+      wait ${job_pids[0]}						      # Wait for the first job to finish (blocking wait for one job)
       job_pids=("${job_pids[@]:1}")  			# Remove the finished job's PID from the array
-      jobs_running=$((jobs_running - 1))  	# Decrease the job count after one finishes
+      jobs_running=$((jobs_running - 1))  # Decrease the job count after one finishes
     fi
   done
 fi
